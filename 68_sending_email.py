d349d08460575662705103178923d83c99e3eb80
Agata Orzechowska <agata9orzechowska@gmail.com>
2020-01-14 16:15:47 +0100

    Sending mail in function

--- 68_sending_email.py
+++ 68_sending_email.py
@@ -1,4 +1,27 @@
 import smtplib
+import functools
+
+
+def send_email(user, password, mailFrom, mailTo, mailSubject, mailBody):
+
+    message = '''From: {}
+    Subject: {}
+    
+    {}
+    '''.format(mailFrom, mailSubject, mailBody)
+
+    try:
+        server = smtplib.SMTP_SSL('smtp.gmail.com', 465)
+        server.ehlo()
+        server.login(user, password)
+        server.sendmail(user, mailTo, message)
+        server.close()
+        print('mail sent')
+        return True
+    except:
+        print('error sending email')
+        return False
+
 
 mailFrom = 'Harry Potter'
 mailTo = ['spamerka666@gmail.com']
@@ -7,22 +30,8 @@ mailBody = '''
 AIN EINGARP ACRESO GEWTEL AZ RAWTA WTE IN MAJ IBDO,
 
 You-know-who!'''
-
-message = '''From: {}
-Subject: {}
-
-{}
-'''.format(mailFrom, mailSubject, mailBody)
-
 user = 'spamerka666@gmail.com'
 password = 'qwer4321!'
 
-try:
-    server = smtplib.SMTP_SSL('smtp.gmail.com', 465)
-    server.ehlo()
-    server.login(user, password)
-    server.sendmail(user, mailTo, message)
-    server.close()
-    print('mail sent')
-except:
-    print('error sending email')
+send_email_from_gmail = functools.partial(send_email, user, password, mailSubject="Execution")
+send_email_from_gmail(mailFrom=mailFrom, mailTo=mailTo, mailBody=mailBody)
