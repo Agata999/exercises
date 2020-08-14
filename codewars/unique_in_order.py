7d66c9c4d1c57691c8e6a5e3f506eaac5cbbc2c9
Agata Orzechowska <agata9orzechowska@gmail.com>
2020-08-14 21:02:02 +0200

    refacto solution - add an unique character to the list from the string

--- codewars/unique_in_order.py
+++ codewars/unique_in_order.py
@@ -1,10 +1,10 @@
 def unique_in_order(iterable):
     unique_list = []
-    if iterable.count(iterable[0]) == len(iterable):
-        unique_list.append(iterable[0])
-    for i in range(0, len(iterable)):
-        if iterable[i-1] != iterable[i]:
-            unique_list.append(iterable[i])
+    prev = None
+    for char in iterable:
+        if char != prev:
+            unique_list.append(char)
+        prev = char
     return unique_list
 
 
